ea9b4baed9ab0c278fd924a548d9027c4defb5f8
youngwasd <youngk126621@gmail.com>
2024-01-16 15:38:15 -0800

    camera bug

--- theProtagonist.js
+++ theProtagonist.js
@@ -8,11 +8,14 @@ class TheProtagonist {
 
         this.x = 0;
         this.y = 0;
-        this.speed = 50;
+
+        this.cameraX = 0;
+        this.cameraY = 0;
+        this.speed = 0.5;
     }
 
     update() {
-        const elapsed = this.game.clockTick;
+        //const elapsed = this.game.clockTick;
 
         // Reset movement values
         let deltaX = 0;
@@ -20,35 +23,38 @@ class TheProtagonist {
         
         // Check individual directions
         if (this.game.left) {
-            deltaX -= this.speed * elapsed;
+            deltaX -= this.speed;
         }
 
         if (this.game.right) {
-            deltaX += this.speed * elapsed;
+            deltaX += this.speed;
         }
 
         if (this.game.up) {
-            deltaY -= this.speed * elapsed;
+            deltaY -= this.speed;
         }
 
         if (this.game.down) {
-            deltaY += this.speed * elapsed;
+            deltaY += this.speed;
         }
 
         // Normalize the movement vector
         const length = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
         if (length !== 0) {
-            const normalizedDeltaX = (deltaX / length) * this.speed * elapsed;
-            const normalizedDeltaY = (deltaY / length) * this.speed * elapsed;
+            const normalizedDeltaX = (deltaX / length) * this.speed;
+            const normalizedDeltaY = (deltaY / length) * this.speed;
 
             // Update position
             this.x += normalizedDeltaX;
             this.y += normalizedDeltaY;
         }
-    }
+     }
 
     draw(ctx) {
         ctx.drawImage(this.bgd, 0, 0);
         this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+        ctx.setTransform(1, 0, 0, 1, -this.cameraX, -this.cameraY);
+        this.cameraX = this.x - ctx.canvas.width / 2;
+        this.cameraY = this.y - ctx.canvas.height / 2;
     }
 }
