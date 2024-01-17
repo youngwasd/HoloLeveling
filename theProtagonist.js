d9504db738fb6da6c631d058b083a85d6a8fd031
youngwasd <youngk126621@gmail.com>
2024-01-16 16:58:15 -0800

    fixing camera and map stuff 2.0

--- theProtagonist.js
+++ theProtagonist.js
@@ -5,21 +5,6 @@ class TheProtagonist {
         this.spritesheet = ASSET_MANAGER.getAsset("./sprites/test.png");
         this.bgd = ASSET_MANAGER.getAsset("./sprites/forest.png");
 
-<<<<<<< Updated upstream
-        this.animator = new Animator(this.spritesheet, 0, 0, 45, 80, 3, 0.8);
-
-        this.x = 0;
-        this.y = 0;
-
-        this.cameraX = 0;
-        this.cameraY = 0;
-        this.speed = 0.5;
-    }
-
-    update() {
-        //const elapsed = this.game.clockTick;
-
-=======
         this.animator = new Animator(this.spritesheet, 0, 0, 45, 80, 3, 0.8, 2);
 
         this.x = 0;
@@ -32,27 +17,11 @@ class TheProtagonist {
     }
 
     update() {
->>>>>>> Stashed changes
         // Reset movement values
         let deltaX = 0;
         let deltaY = 0;
         
         // Check individual directions
-<<<<<<< Updated upstream
-        if (this.game.left) {
-            deltaX -= this.speed;
-        }
-
-        if (this.game.right) {
-            deltaX += this.speed;
-        }
-
-        if (this.game.up) {
-            deltaY -= this.speed;
-        }
-
-        if (this.game.down) {
-=======
         if (this.game.left && this.x > 0) {
             deltaX -= this.speed;
         }
@@ -66,7 +35,6 @@ class TheProtagonist {
         }
 
         if (this.game.down && this.y < this.mapHeight - this.animator.height) {
->>>>>>> Stashed changes
             deltaY += this.speed;
         }
 
@@ -92,13 +60,6 @@ class TheProtagonist {
      }
 
     draw(ctx) {
-<<<<<<< Updated upstream
-        ctx.drawImage(this.bgd, 0, 0);
-        this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
-        ctx.setTransform(1, 0, 0, 1, -this.cameraX, -this.cameraY);
-        this.cameraX = this.x - ctx.canvas.width / 2;
-        this.cameraY = this.y - ctx.canvas.height / 2;
-=======
         this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
     
         // Calculate the center position of the character
@@ -107,6 +68,5 @@ class TheProtagonist {
     
         // Set the transformation to center the camera on the character's center
         ctx.setTransform(1, 0, 0, 1, ctx.canvas.width / 2 - centerX, ctx.canvas.height / 2 - centerY);
->>>>>>> Stashed changes
     }
 }
