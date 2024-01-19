22227e89e206671164a0948f053a8fe915610516
JohnVictor874 <joao.zuccati@gmail.com>
2024-01-19 01:00:25 -0800

    merge attempt with main

--- SceneManager.js
+++ SceneManager.js
@@ -11,15 +11,23 @@ class SceneManager{
         let background = new Map(this.gameEngine, 0, 0, 2500, 2500, 1);
 
         this.theProtagonist = new TheProtagonist(this.gameEngine, background);
-        this.theProtagonist.x = 1000;
-        this.theProtagonist.y = 1000;
-
-        this.enemy = new Enemy(this.gameEngine, 1000, 1000);
+        this.theProtagonist.x = 500;
+        this.theProtagonist.y = 500;
+        
         
         this.tree = new Tree(900, 1000, this.theProtagonist);
 
-        this.gameEngine.addEntity(this.theProtagonist, background);
-        //this.gameEngine.addEntity(this.enemy);
+        for (let i = 0; i < 10; i++) {
+            const x = Math.floor(Math.random() * background.getWidth());
+            const y = Math.floor(Math.random() * background.getHeight());
+
+            let speed = Math.floor(Math.random() * (this.theProtagonist.speed * 0.8) + 150);
+            speed = speed >= this.theProtagonist.speed ? speed - 100 : speed;
+
+            this.enemy = new Enemy(this.gameEngine, x, y, speed);
+            this.gameEngine.addEntity(this.enemy);
+        }
+
         this.gameEngine.addEntity(this.tree);
         this.gameEngine.addEntity(background);
     }
