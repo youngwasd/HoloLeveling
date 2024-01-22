870e51885c74b091b69c791b5c35a2eba69a8573
JohnVictor874 <joao.zuccati@gmail.com>
2024-01-22 15:58:37 -0800

    all bugs fixed

--- SceneManager.js
+++ SceneManager.js
@@ -13,21 +13,13 @@ class SceneManager{
         this.theProtagonist = new TheProtagonist(this.gameEngine, background);
         this.theProtagonist.x = 1000;
         this.theProtagonist.y = 1000;
-        
+        let garlic = new Garlic(this.gameEngine, this.theProtagonist);
         this.tree = new Tree(900, 1000, this.theProtagonist);
-
-        this.gameEngine.addEntity(this.theProtagonist, background);
-
-        for (let i = 0; i < 10; i++) {
-            const x = Math.floor(Math.random() * background.getWidth());
-            const y = Math.floor(Math.random() * background.getHeight());
-            
-            let speed = Math.floor(Math.random() * (this.theProtagonist.speed * 0.8) + 150);
-            speed = speed >= this.theProtagonist.speed ? speed - 100 : speed;
-
-            this.enemy = new Enemy2(this.gameEngine, x, y, speed,this.theProtagonist);
-            this.gameEngine.addEntity(this.enemy);
-        }
+        
+        this.gameEngine.addEntity(this.theProtagonist);
+        this.gameEngine.addEntity(garlic);
+        this.enemy = new Enemy2(800, 1000,this.theProtagonist,garlic);
+        this.gameEngine.addEntity(this.enemy);
 
         this.gameEngine.addEntity(this.tree);
         this.gameEngine.addEntity(background);
