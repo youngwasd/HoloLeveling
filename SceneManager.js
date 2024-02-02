f6b3333256fafe6ba1179f01ce4da85858b5ee8f
JohnVictor874 <joao.zuccati@gmail.com>
2024-02-01 21:39:09 -0800

    young and jv stuff works together

--- SceneManager.js
+++ SceneManager.js
@@ -7,10 +7,12 @@ class SceneManager {
     }
 
     loadLevelOne() {
+        this.end = new EndScreen(this.game);
         this.background = new Map(this.game, 0, 0, 2500, 2500);
-        this.theProtagonist = new TheProtagonist(this.game, this.background);
-
+        this.theProtagonist = new TheProtagonist(this.game, this.background, this.end);
+        
         this.game.addEntity(this.theProtagonist);
+        this.game.addEntity(this.end);
         this.game.addEntity(new Dagger(this.game, this.theProtagonist));
         this.game.addEntity(new Tree(this.game, 900, 1000));
         this.game.addEntity(new Wave(this.game, this.theProtagonist));
