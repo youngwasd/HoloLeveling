fb5e362938d93c539bfff8717e930f095968b577
JohnVictor874 <70738388+JohnVictor874@users.noreply.github.com>
2024-02-16 10:53:50 -0800

    Update animator.js

--- animator.js
+++ animator.js
@@ -8,13 +8,18 @@ class Animator {
     };
 
     drawFrame(tick, ctx, x, y) {
-        this.elapsedTime += tick;
+    if (!this.spriteSheet.complete || this.spriteSheet.naturalHeight === 0) {
+        console.error('Sprite sheet not loaded');
+        return; // Do not proceed if the image is not loaded
+    }
 
-        if (this.elapsedTime > this.totalTime) this.elapsedTime -= this.totalTime;
-        const frame = this.currentFrame();
-        const frameX = this.isReversed ? this.frames - frame - 1 : frame;
-        ctx.drawImage(this.spriteSheet, this.xStart + this.width * frameX, this.yStart, this.width, this.height, x, y, this.width * this.scale, this.height * this.scale);
-    };
+    this.elapsedTime += tick;
+
+    if (this.elapsedTime > this.totalTime) this.elapsedTime -= this.totalTime;
+    const frame = this.currentFrame();
+    const frameX = this.isReversed ? this.frames - frame - 1 : frame;
+    ctx.drawImage(this.spriteSheet, this.xStart + this.width * frameX, this.yStart, this.width, this.height, x, y, this.width * this.scale, this.height * this.scale);
+}
 
     currentFrame() {
         return Math.floor(this.elapsedTime / this.duration);
