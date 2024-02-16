1ef85f836747d2ae7ba260b50cd815820f44f977
youngwasd <youngk126621@gmail.com>
2024-02-15 19:26:18 -0800

    random stuff

--- animator.js
+++ animator.js
@@ -5,7 +5,7 @@ class Animator {
         this.elapsedTime = 0;
         this.totalTime = duration * frames;
         this.isReversed = false;
-    }
+    };
 
     drawFrame(tick, ctx, x, y) {
         this.elapsedTime += tick;
@@ -14,15 +14,17 @@ class Animator {
         const frame = this.currentFrame();
         const frameX = this.isReversed ? this.frames - frame - 1 : frame;
         ctx.drawImage(this.spriteSheet, this.xStart + this.width * frameX, this.yStart, this.width, this.height, x, y, this.width * this.scale, this.height * this.scale);
-    }
+    };
 
     currentFrame() {
         return Math.floor(this.elapsedTime / this.duration);
-    }
+    };
 
     reverse() {
         this.isReversed = !this.isReversed;
-    }
+    };
 
-    isDone() {}
-}
+    isDone() {
+        return this.elapsedTime >= this.totalTime;
+    };
+};
